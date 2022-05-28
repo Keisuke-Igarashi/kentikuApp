fd4b7c3feb0ac6136c44cc15d516351261e4e115
Keisuke-Igarashi <igarashi4948@gmail.com>
2022-05-28 21:04:13 +0900

    created /blog/index.html modified blog.py

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -13,5 +13,8 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, '
-    )
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
